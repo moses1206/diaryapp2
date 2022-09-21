2e3666a863959920379b1f8bc9da96265ed7e0a8
moses1206 <mose233@gmail.com>
2022-09-21 16:48:47 +0900

    first init

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hi</Text>
+    </View>
+  );
+};
+
+export default App;
